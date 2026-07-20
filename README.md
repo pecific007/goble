7c5427f42236d4e0c69e1f8acb6959b130670b1d
pecific007 <pecific2007@gmail.com>
2026-07-20 21:39:51 +0530

    Implementation complete

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # Goble
 
 It's a wrapper of [`bluetoothctl`](https://man.archlinux.org/man/bluetoothctl.1) (I can't write anything of my own).
+
+### Usage:
+```sh
+$  ./goble
+Usage of ./goble:
+  -c	Connect to bluetooth device
+  -d	Disconnect from bluetooth device
+  -s	Connect to sister's earphones
+```
+
+``` sh
+$ ./goble -c
+Attempting to connect to 8C:D3:D9:F7:62:D0
+Connection successful
+[CHG] Device 8C:D3:D9:F7:62:D0 Trusted: yes
+Changing 8C:D3:D9:F7:62:D0 trust succeeded
+Connected and trusted: 8C:D3:D9:F7:62:D0%
+```
+
+```sh
+$ go build && ./goble -d
+Attempting to disconnect from 8C:D3:D9:F7:62:D0
+[CHG] Device 8C:D3:D9:F7:62:D0 ServicesResolved: no
+[SIGNAL] BREDR.Disconnected - org.bluez.Reason.Remote, Connection terminated by remote user
+[SIGNAL] Disconnected - org.bluez.Reason.Remote, Connection terminated by remote user
+[CHG] Device 8C:D3:D9:F7:62:D0 Connected: no
+Disconnection successful
+[CHG] Device 8C:D3:D9:F7:62:D0 Trusted: no
+Changing 8C:D3:D9:F7:62:D0 untrust succeeded
+Disconnected and untrusted: 8C:D3:D9:F7:62:D0%
+```
+```
+```
--- main.go
+++ main.go
@@ -1,7 +1,86 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	connect := flag.Bool("c", false, "Connect to bluetooth device")
+	disconnect := flag.Bool("d", false, "Disconnect from bluetooth device")
+	sister := flag.Bool("s", false, "Connect to sister's earphones")
+
+	flag.Parse()
+
+	if len(os.Args) == 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error loaindg .env file: %v", err)
+		os.Exit(1)
+	}
+
+	boat := os.Getenv("boat")
+	realme := os.Getenv("realme")
+
+	var deviceMacAddr string
+	if *sister {
+		deviceMacAddr = realme
+	} else {
+		deviceMacAddr = boat
+	}
+
+	if *connect == *disconnect {
+		fmt.Fprintln(os.Stderr, "Cannot pass -c (connect) and -d (disconnect) both.")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if *connect {
+		if err := runBluetoothCtl("connect", deviceMacAddr); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to connect %s\n", err)
+			os.Exit(1)
+		}
+		if err := runBluetoothCtl("trust", deviceMacAddr); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to trust %s\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Connected and trusted: %v", deviceMacAddr)
+	}
+	if *disconnect {
+		if err := runBluetoothCtl("disconnect", deviceMacAddr); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to disconnect %s\n", err)
+			os.Exit(1)
+		}
+		if err := runBluetoothCtl("untrust", deviceMacAddr); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to untrust %s\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Disconnected and untrusted: %v", deviceMacAddr)
+	}
+}
+
+func runBluetoothCtl(action, macAddr string) error {
+	cmd := exec.Command("bluetoothctl", action, macAddr)
+	out, err := cmd.Output()
+	output := strings.TrimSpace(string(out))
+	if output != "" {
+		fmt.Println(output)
+	}
+	if err != nil {
+		return fmt.Errorf("%v (output: %s)", err, output)
+	}
+
+	if strings.Contains(output, "failed") {
+		return fmt.Errorf("bluetoothctl reported failure: %s", output)
+	}
+	return nil
 }
